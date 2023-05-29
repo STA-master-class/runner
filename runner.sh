9cf15a6b38f13d8e07ab6b4f9b4d680d285e5476
zina mevzos <zina.m@coralogix.com>
2023-05-29 14:27:21 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-for i in `seq 1 1 3`
+for i in {1..10}
 do
-	for i in {1..10}
-	do
-		touch ${i}.txt
-	done
-	for i in {1..10}
-	do
-		mv ${i}.txt ${i}.txt.1
-	done
-	
-	sleep 7200
+	touch ${i}.txt
+done
+for i in {1..10}
+do
+	mv ${i}.txt ${i}.txt.1
 done
